6c0efe67e33024809bc156ac3d4d45a89c3efe61
Osamuyi <muyixone@yahoo.com>
2023-02-02 13:42:56 +0100

    Project setup

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require('cors');
+
+//ROUTES
+const indexRouter = require('./routes/index');
+const userRouter = require('./routes/user');
+const chatRoomRouter = require('./routes/chatRoom');
+const deleteRouter = require('./routes/delete');
+
+//MDDLEWARE
+const authenticationMiddleware = require('./middleware/jwt');
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use('/', indexRouter);
+app.use('/users', userRouter);
+app.use('/room', chatRoomRouter);
+app.use('/delete', deleteRouter);
+
+// ERROR HANDLER
+app.use('*', (req, res, next) => {
+  return res.status(404).json({
+    sucess: false,
+    message: 'End point does not exist',
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
